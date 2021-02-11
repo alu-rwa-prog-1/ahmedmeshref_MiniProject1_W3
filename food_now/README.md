11334b9567dcd78cbaccb53328d6292473abb0c7
ahmedmeshref <a.meshref@alustudent.com>
2021-02-11 21:40:35 +0200

    Update readme && Update mainView

--- food_now/README.md
+++ food_now/README.md
@@ -2,15 +2,9 @@
 
 A new Flutter application for online food shopping.
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Resources:
+- Stack Overflow - Where Developers Learn, Share, & Build Careers. (2021). Retrieved 11 February 2021, from https://stackoverflow.com/
+    - https://stackoverflow.com/questions/61909064/how-to-overlay-icon-on-image-in-flutter
+    - https://stackoverflow.com/questions/45669202/how-to-add-a-listview-to-a-column-in-flutter#:~:text=Column%20expands%20to%20the%20maximum,and%20so%20does%20the%20ListView%20.&text=So%2C%20you%20need%20to%20constrain%20the%20height%20of%20the%20ListView%20.&text=If%20you%20want%20to%20allow,space%20inside%20Column%20use%20Expanded%20.
+- Flutter Icon Color. (2021). Retrieved 11 February 2021, from https://googleflutter.com/flutter-icon-color/#:~:text=Flutter%20Icon%20with%20Specific%20Color&text=You%20can%20change%20the%20color%20of%20an%20Icon%2C%20and%20you,any%20of%20such%20allowed%20way.
+- Flutter documentation. (2021). Retrieved 11 February 2021, from https://flutter.dev/docs
--- food_now/lib/home/components/body.dart
+++ food_now/lib/home/components/body.dart
@@ -6,8 +6,9 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(9.0),
-      child: Column(
+      child: ListView(
         children: [
+          // Categories section
           Row(
             children: [
               Expanded(
@@ -30,6 +31,7 @@ class Body extends StatelessWidget {
               ),
             ],
           ),
+          // Food special section
           Row(
             children: [
               Expanded(
@@ -80,6 +82,116 @@ class Body extends StatelessWidget {
                     'https://i2.wp.com/www.foodrepublic.com/wp-content/uploads/2012/05/testkitchen_argentinesteak.jpg?resize=1280%2C%20560&ssl=1',
                     discount: '20',
                     favourite: false
+                ),
+              ],
+            ),
+          ),
+          // Drinks section
+          Row(
+            children: [
+              Expanded(
+                  child: Text(
+                    'Drinks',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                  )),
+              RaisedButton(
+                elevation: 0,
+                padding: EdgeInsets.all(0),
+                onPressed: () {},
+                color: Colors.white30,
+                textColor: Theme.of(context).primaryColor,
+                child: Row(
+                  children: <Widget>[
+                    Text('View all'),
+                    Icon(Icons.keyboard_arrow_right)
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Container(
+            height: 215,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                CardItem(
+                    title: 'Cola',
+                    price: '200',
+                    imageURL:
+                    'https://www.mcdonalds.eg/Cms_Data/Contents/En/Media/images/Menu/large-Image/Coke.png',
+                    discount: '2',
+                    favourite: true
+                ),
+                CardItem(
+                    title: 'Pepsi',
+                    price: '500',
+                    imageURL:
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQPJuhvo1InQPcUo1xMHC8i9zIy8reHk4DJw&usqp=CAU',
+                    discount: '2',
+                    favourite: false
+                ),
+                CardItem(
+                    title: 'Lemon Juice',
+                    price: '2000',
+                    imageURL:
+                    'https://homepuff.com/wp-content/uploads/2019/04/lemon-juice.jpg',
+                    discount: '4',
+                    favourite: false
+                ),
+              ],
+            ),
+          ),
+          // Snacks section
+          Row(
+            children: [
+              Expanded(
+                  child: Text(
+                    'Snacks',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                  )),
+              RaisedButton(
+                elevation: 0,
+                padding: EdgeInsets.all(0),
+                onPressed: () {},
+                color: Colors.white30,
+                textColor: Theme.of(context).primaryColor,
+                child: Row(
+                  children: <Widget>[
+                    Text('View all'),
+                    Icon(Icons.keyboard_arrow_right)
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Container(
+            height: 215,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                CardItem(
+                    title: 'Samosa',
+                    price: '300',
+                    imageURL:
+                    'https://www.harkoichef.com/wp-content/uploads/2019/06/samos.png',
+                    discount: '4',
+                    favourite: false
+                ),
+                CardItem(
+                    title: 'Chips',
+                    price: '50000',
+                    imageURL:
+                    'https://fishandcheapsfood.files.wordpress.com/2018/09/fish-and-cheap-3.jpg?w=376&h=245',
+                    discount: '3',
+                    favourite: true
+                ),
+                CardItem(
+                    title: 'Tea',
+                    price: '100',
+                    imageURL:
+                    'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/tea-royalty-free-image-1590530043.jpg?crop=1.00xw:0.752xh;0,0.118xh&resize=1200:*',
+                    discount: '4',
+                    favourite: true
                 ),
               ],
             ),
